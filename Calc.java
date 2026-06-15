c57ec82ae8aaad00b9c159dbe5363bd889a8fac1
suntech\c23004 <c23004@suntech.jp>
2026-06-15 15:53:44 +0900

    減算機能を追加しました

--- Calc.java
+++ Calc.java
@@ -5,6 +5,9 @@ public class Calc{
     public static int add(int a,int b){
         return a+b;
     }
+    public static int sub(int a,int b){
+        return a-b;
+    }
 
     public static void main(String[] args){
         Scanner stdIn = new Scanner(System.in);
@@ -12,6 +15,6 @@ public class Calc{
         int b=stdIn.nextInt();
 
         System.out.println("和: "+add(a,b));
-        //System.out.println("差: "+sub(a,b));
+        System.out.println("差: "+sub(a,b));
     }
 }
