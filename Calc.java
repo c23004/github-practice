d093690faf1b7f953138288326623981d2c54430
suntech\c23004 <c23004@suntech.jp>
2026-06-22 13:26:56 +0900

    加算機能を追加

---
+++ Calc.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Calc{
+    public static int add(int a,int b){
+        return a+b;
+    }
+    // public static int sub(int a,int b){
+    //     int ans=a-b;
+    //     return ans;
+    // }
+
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int a=stdIn.nextInt();
+        int b=stdIn.nextInt();
+
+        System.out.println("和: "+add(a,b));
+        //System.out.println("差: "+sub(a,b));
+    }
+}
